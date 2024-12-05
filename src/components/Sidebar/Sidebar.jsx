0019fbedf482a709f316dcc69c04b6e471df39fa
unknown <abhishekmishra992016@gmail.com>
2024-12-05 17:53:18 +0530

    side bar style

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -34,13 +34,17 @@ const Sidebar = () => {
                 <div className="overflow-y-auto py-5 px-3 h-full  bg-[#1C2333] dark:border-gray-700">
                     <ul className="space-y-2">
                         <li>
-                            <Link to="/" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
-                                <svg aria-hidden="true" className="w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-                                <span className="ml-3 ">REPLIT</span>
+                            <Link to="/" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg border border-transparent dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 active:border-sky-300 group">
+                                <svg aria-hidden="true" className="w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                                    <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                                </svg>
+                                <span className="ml-3">REPLIT</span>
                             </Link>
+
                         </li>
                         <li>
-                            <Link to="/" className="flex items-center text-center p-1 font-normal border text-gray-900 border-gray-200 dark:border-gray-700 rounded-md dark:text-white hover:bg-gray-800 dark:hover:bg-gray-800 group">
+                            <Link to="/" className="flex items-center text-center p-1 font-normal border text-gray-900 border-gray-200 dark:border-gray-700 rounded-md dark:text-white hover:bg-gray-800 dark:hover:bg-gray-800 group active:border-sky-300">
                                 <span className="ml-8 flex justify-center items-center text-center gap-2">
                                     <LuPlus />
                                     <p className="text-center"> Create Repl</p>
@@ -48,27 +52,33 @@ const Sidebar = () => {
                             </Link>
                         </li>
                         <li>
-                            <Link to="/"
-                                className={`flex items-center p-2 w-full text-base font-normal text-white rounded-lg transition duration-75 group 
-                        ${location?.pathname === '/' ? "bg-gray-200 text-white dark:bg-gray-700" : "hover:bg-gray-200 dark:hover:bg-gray-700"}
-                             `}
+                            <Link
+                                to="/"
+                                className={`flex items-center p-2 w-full text-base font-normal text-white  rounded-lg transition duration-75 group active:border-sky-300
+                              ${location?.pathname === '/' ? "bg-gray-200 text-white dark:bg-gray-700 border-sky-300" : "hover:bg-gray-200 dark:hover:bg-gray-700"}`}
                                 aria-controls="dropdown-pages"
-                                data-collapse-toggle="dropdown-pages">
-                                <svg aria-hidden="true"
+                                data-collapse-toggle="dropdown-pages"
+                            >
+                                <svg
+                                    aria-hidden="true"
                                     className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                                     fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fillRule="evenodd"
                                         d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-                                        clipRule="evenodd"></path>
+                                        clipRule="evenodd"
+                                    ></path>
                                 </svg>
                                 <span className="flex-1 ml-3 text-xs text-left whitespace-nowrap">Home</span>
                             </Link>
 
                         </li>
                         <li>
-                            <Link to="/editor" className={`flex items-center p-2 w-full text-base font-normal text-white rounded-lg transition duration-75 group 
-                        ${location?.pathname === '/editor' ? "bg-gray-200 text-white dark:bg-gray-700" : "hover:bg-gray-200 dark:hover:bg-gray-700"}
+                            <Link to="/editor" className={`flex items-center p-2 w-full text-base font-normal text-white rounded-lg transition duration-75   group  
+                        ${location?.pathname === '/editor' ? "bg-gray-200 text-white dark:bg-gray-700 active:border-sky-300" : "hover:bg-gray-200 dark:hover:bg-gray-700 "}
                              `} aria-controls="dropdown-sales" data-collapse-toggle="dropdown-sales">
                                 <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd"></path></svg>
                                 <span className="flex-1 ml-3 text-xs text-left whitespace-nowrap">Repls</span>
--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -5,27 +5,12 @@ import LanguageSelector from "../components/Editor/LangaugeSelector";
 import Terminal from "../components/Terminal/Terminal";
 
 const EditorPage = () => {
-  const dispatch = useDispatch();
-  const { code, language } = useSelector((state) => state.editor);
 
-  const runCode = async () => {
-    const response = await fetch("/api/execute", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ code, language }),
-    });
-    const result = await response.json();
-    dispatch(setOutput(result.output || "Error"));
-  };
+
 
   return (
     <div className="editor-page flex flex-col gap-4 p-4">
-      <LanguageSelector />
-      <CodeEditor />
-      <button onClick={runCode} className="btn bg-blue-500 text-white p-2 rounded">
-        Run Code
-      </button>
-      <Terminal />
+
     </div>
   );
 };
