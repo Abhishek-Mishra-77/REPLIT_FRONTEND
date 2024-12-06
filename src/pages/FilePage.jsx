64eb8b10fe5b0ce2982c37cd2b334fadd4e30d73
unknown <abhishekmishra992016@gmail.com>
2024-12-06 17:59:15 +0530

    started working on file seciton

--- src/pages/FilePage.jsx
+++ src/pages/FilePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import TopBar from '../components/TopBar/TopBar'
 import File from '../components/File/File'
+import { useSelector } from 'react-redux'
 
 const FilePage = () => {
+    const { home, file } = useSelector((state) => state);
+
     return (
         <div>
-            <TopBar />
+            <TopBar Repls={file.files} />
             <div className="p-12 text-white">
                 <File />
             </div>
