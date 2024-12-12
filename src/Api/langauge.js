9ac726ce730ab9de82806ac01778fc76917cb456
unknown <abhishekmishra992016@gmail.com>
2024-12-12 15:13:24 +0530

    started working langauge frontend and file

---
+++ src/Api/langauge.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { serverUrl } from "../services/common";
+import { toast } from "react-toastify";
+
+const token = localStorage.getItem("token");
+
+const onGetAllLanguagesHandler = async () => {
+    const token = localStorage.getItem("token");
+    try {
+        const response = await axios.get(`${serverUrl}/languages`, { headers: { "Authorization": `Bearer ${token}` } });
+        return response.data;
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+
+const onCreateLanguageHandler = async (name) => {
+    const token = localStorage.getItem("token");
+    try {
+        const response = await axios.post(`${serverUrl}/languages/create`, { name }, { headers: { "Authorization": `Bearer ${token}` } });
+        return response.data;
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+
+
+export { onGetAllLanguagesHandler, onCreateLanguageHandler }
--- src/components/File/File.jsx
+++ src/components/File/File.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import Header from '../Home/Header'
 
 const File = ({ folders }) => {
-
-
-
     return (
         <div className="text-white gap-2 rounded-lg">
-            <Header data={folders} />
+            <Header data={folders} name="Files" />
         </div>
     )
 }
